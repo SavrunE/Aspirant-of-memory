870a447e406d564ba3ce72bd2996a5282fc577c3
SavrunE <igor1996igor@inbox.ru>
2021-02-15 22:58:57 +0300

    bad lerp

--- Aspirant-of-memory/Assets/Scripts/Buttons/ButtonEffect.cs
+++ Aspirant-of-memory/Assets/Scripts/Buttons/ButtonEffect.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class ButtonEffect : MonoBehaviour, IButtonEffect
 {
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
     public void Activate() 
     {
         Debug.Log("Activated");
+        sprite.color = Color.Lerp(sprite.color, 
+            new Color(sprite.color.r, sprite.color.g, sprite.color.b, 255), 
+            0.001f);
+        
     }
 }
