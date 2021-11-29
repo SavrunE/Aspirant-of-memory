be291c800beb73e2943f6883f0476a685fba9aa0
SavrunE <igor1996igor@inbox.ru>
2021-11-29 22:57:56 +0300

    rotate level text price

--- Aspirant-of-memory/Assets/Scripts/Scenes/GameMods/LevelSistem/NoOpenLevelViewer.cs
+++ Aspirant-of-memory/Assets/Scripts/Scenes/GameMods/LevelSistem/NoOpenLevelViewer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class NoOpenLevelViewer : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class NoOpenLevelViewer : MonoBehaviour
             image.sprite = CloseImageSprite;
             //Text how many must pay to open
             text.text = "sad";
+            text.transform.DORotate(new Vector3(0,0,45),0,RotateMode.Fast);
         }
     }
 }
